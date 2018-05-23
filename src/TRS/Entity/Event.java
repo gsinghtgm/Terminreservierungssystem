9b7fd84a19dc08375e330a2d5ac45fd330367086
Gurparkash Singh <gsingh@student.tgm.ac.at>
2018-05-23 11:37:42 +0200

    Basis Klassen für Entity

--- src/TRS/Entity/Event.java
+++ src/TRS/Entity/Event.java
@@ -1,5 +1,84 @@
-package TRS;
+package TRS.Entity;
+
+import java.util.List;
 
 public class Event {
+
+	private int id;
+	
+	private User creator;
+
+	private String name;
+
+	private String location;
+
+	private String note;
+
+	private List<Comment> comments;
+	
+	private List<Participant> participating;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	public User getCreator() {
+		return creator;
+	}
+	
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+
+	
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+	public List<Participant> getParticipating() {
+		return participating;
+	}
+
+	public void setParticipating(List<Participant> participating) {
+		this.participating = participating;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [id=" + id + ", creator=" + creator + ", name=" + name + ", location=" + location + ", note="
+				+ note + "]";
+	}
 
 }
--- src/TRS/Entity/User.java
+++ src/TRS/Entity/User.java
@@ -1,5 +1,91 @@
-package TRS;
+package TRS.Entity;
 
+import java.util.List;
 public class User {
+
+	private int id;
+	private String firstname;
+
+	private String lastname;
+
+	private String email;
+
+	private String password;
+
+	private List<Event> events;
+
+	private List<Comment> comments;
+
+	private List<Participant> participating;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+	
+	public List<Participant> getParticipating() {
+		return participating;
+	}
+
+	public void setParticipating(List<Participant> participating) {
+		this.participating = participating;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email
+				+ ", password=" + password + "]";
+	}
 
 }
