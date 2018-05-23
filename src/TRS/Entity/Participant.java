ee9e82b0229b4672b8efe75a13a25c1e25b40c92
Semir Alagic <salagic@student.tgm.ac.at>
2018-05-23 12:17:31 +0200

    participant entity

--- src/TRS/Entity/Participant.java
+++ src/TRS/Entity/Participant.java
@@ -1,11 +1,26 @@
 package TRS.Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "participant")
 public class Participant {
 
+	@Id
+	@Column(name = "id")
 	private int id;
 	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
 	private User participant;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "event_id")
 	private Event event;
 
 	public User getParticipant() {
